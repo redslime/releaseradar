f2bc5d285d93ea1954340ccefbaac8238cc2b171
redslime <hallopiu@gmail.com>
2023-08-14 18:05:29 +0200

    Update /collectreminders to skip over no-read channels

--- src/main/kotlin/xyz/redslime/releaseradar/command/CollectRemindersCommand.kt
+++ src/main/kotlin/xyz/redslime/releaseradar/command/CollectRemindersCommand.kt
@@ -12,6 +12,7 @@ import dev.kord.rest.builder.message.modify.embed
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.takeWhile
 import kotlinx.coroutines.flow.toList
+import org.apache.logging.log4j.LogManager
 import xyz.redslime.releaseradar.cache
 import xyz.redslime.releaseradar.success
 import xyz.redslime.releaseradar.util.addPostLater
@@ -23,6 +24,8 @@ import xyz.redslime.releaseradar.util.getStartOfToday
  * @version 2023-06-15
  */
 class CollectRemindersCommand: AdminCommand("collectreminders", "Looks for reminders globally") {
+
+    private val logger = LogManager.getLogger(javaClass)
 
     override fun addParameters(builder: ChatInputCreateBuilder) {
 
@@ -44,20 +47,24 @@ class CollectRemindersCommand: AdminCommand("collectreminders", "Looks for remin
                 ch.asChannelOrNull()?.let { channel ->
                     if(channel is MessageChannelBehavior) {
                         channel.data.lastMessageId.value?.let { lastMessage ->
-                            channel.getMessagesBefore(lastMessage)
-                                .takeWhile { it.timestamp > today }
-                                .filter { it.author == interaction.kord.getSelf() }
-                                .toList()
-                                .forEach { message ->
-                                    extractSpotifyLink(message)?.let { url ->
-                                        message.getReactors(alarmEmoji)
-                                            .filter { it != interaction.kord.getSelf() }
-                                            .toList().forEach {
-                                                if(addPostLater(url, it))
-                                                    added++
+                            try {
+                                channel.getMessagesBefore(lastMessage)
+                                    .takeWhile { it.timestamp > today }
+                                    .filter { it.author == interaction.kord.getSelf() }
+                                    .toList()
+                                    .forEach { message ->
+                                        extractSpotifyLink(message)?.let { url ->
+                                            message.getReactors(alarmEmoji)
+                                                .filter { it != interaction.kord.getSelf() }
+                                                .toList().forEach {
+                                                    if (addPostLater(url, it))
+                                                        added++
+                                                }
                                         }
                                     }
-                                }
+                            } catch (ex: Exception) {
+                                logger.error("Failed to get last messages in $id", ex)
+                            }
                         }
                     }
                 }
