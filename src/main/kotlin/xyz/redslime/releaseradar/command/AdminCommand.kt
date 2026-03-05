dd07fdf664fd733532bb7abc094e9ef305fdff0a
redslime <hallopiu@gmail.com>
2026-03-05 18:28:48 +0100

    Move hardcoded snowflake values to config

--- src/main/kotlin/xyz/redslime/releaseradar/command/AdminCommand.kt
+++ src/main/kotlin/xyz/redslime/releaseradar/command/AdminCommand.kt
@@ -5,20 +5,21 @@ import dev.kord.core.Kord
 import org.apache.logging.log4j.LogManager
 import xyz.redslime.releaseradar.PermissionLevel
 import xyz.redslime.releaseradar.commands
+import xyz.redslime.releaseradar.config
 
 /**
  * @author redslime
  * @version 2023-05-22
  */
-private const val ADMIN_SERVER = 546718105595019294L
-
 abstract class AdminCommand(name: String, desc: String): Command(name, desc, perm = PermissionLevel.SERVER_ADMIN) {
 
     override suspend fun register(client: Kord) {
-        LogManager.getLogger(javaClass).info("Registering command ${javaClass.simpleName}")
-        commands.add(this)
-        client.createGuildChatInputCommand(Snowflake(ADMIN_SERVER), name, description) {
-            addParameters(this)
-        }
+        config.adminGuild?.let { gid ->
+            LogManager.getLogger(javaClass).info("Registering command ${javaClass.simpleName}")
+            commands.add(this)
+            client.createGuildChatInputCommand(Snowflake(gid), name, description) {
+                addParameters(this)
+            }
+        } ?: LogManager.getLogger(javaClass).warn("Not registering command ${javaClass.simpleName}, no admin guild set in config!")
     }
 }
--- src/main/kotlin/xyz/redslime/releaseradar/data/Config.kt
+++ src/main/kotlin/xyz/redslime/releaseradar/data/Config.kt
@@ -1,5 +1,6 @@
 package xyz.redslime.releaseradar.data
 
-data class Config(val discordToken: String, val spotifyClientId: String, val spotifySecret: String,
-    val dbDriverName: String, val dbHost: String, val dbUser: String, val dbPassword: String,
-    val redirectUrl: String, val serverPort: Int, val spotifyFallbacks: Map<String, String>)
+data class Config(val discordToken: String, val logChannel: Long?, val adminGuild: Long?, val spotifyClientId: String,
+      val spotifySecret: String, val dbDriverName: String, val dbHost: String, val dbUser: String,
+      val dbPassword: String, val redirectUrl: String, val serverPort: Int, val spotifyFallbacks: Map<String, String>,
+      val saveEmojiGuildId: Long?, val saveEmojiId: Long?)
